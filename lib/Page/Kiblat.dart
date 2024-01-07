69235e32b67b253b0bd78c7335d03101e10f4bbc
Haikal <86674614+Bekicot123@users.noreply.github.com>
2024-01-07 22:10:01 +0700

    update

--- lib/Page/Kiblat.dart
+++ lib/Page/Kiblat.dart
@@ -1,21 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import '../Component/qibla_screen.dart';
 
 
-class Kiblat extends StatelessWidget {
-  Kiblat ({super.key});
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool hasPermission = false;
+
+  Future getPermission() async {
+    if (await Permission.location.serviceStatus.isEnabled) {
+      var status = await Permission.location.status;
+      if (status.isGranted) {
+        hasPermission = true;
+      } else {
+        Permission.location.request().then((value) {
+          setState(() {
+            hasPermission = (value == PermissionStatus.granted);
+          });
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 10,
-      height: 10,
-      color: Colors.blue, // Background color of the container
-      padding: EdgeInsets.all(16), // Padding inside the container
-      margin: EdgeInsets.all(16), // Margin around the container
-      child: Text(
-        'Hello, Container!',
-        style: TextStyle(color: Colors.white, fontSize: 18),
-      ),
-    );
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: FutureBuilder(
+          builder: (context, snapshot) {
+            if (hasPermission) {
+              return const QiblahScreen();
+            } else {
+              return const Scaffold(
+                backgroundColor: Color.fromARGB(255, 48, 48, 48),
+              );
+            }
+          },
+          future: getPermission(),
+        ));
   }
 }
---
+++ lib/Page/NiatSholat.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class niatsholat extends StatelessWidget{
+  niatsholat ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
