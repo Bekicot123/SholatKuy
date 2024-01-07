51c9d7f1d747936abae892abff533a32f06ac5d1
Haikal <86674614+Bekicot123@users.noreply.github.com>
2024-01-08 04:02:25 +0700

    update

--- lib/Page/NiatSholat.dart
+++ lib/Page/NiatSholat.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
 
 class niatsholat extends StatelessWidget{
-  niatsholat ({super.key});
+ const niatsholat ({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  onTap: (){
+                    Navigator.pop(context);
+                  },
+                  child: Icon(Icons.arrow_back_ios_new_outlined,
+                    size: 25,color:Color(hexColor("#334E85")) ,),
+                ),
+              ],
+            ),
+            SizedBox(
+              child: Image.asset("lib/Image/niatsholat.png"),
+            )
+          ],
+        ),
+      ),
+    );
   }
+}
+int hexColor(String color) {
+  String newColor = '0xff' + color;
+  newColor = newColor.replaceAll('#', '');
+  int finalColor = int.parse(newColor);
+  return finalColor;
 }
