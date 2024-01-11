abeca8366bc034d4c81ef016de712f7b0f97be24
Haikal <86674614+Bekicot123@users.noreply.github.com>
2024-01-11 20:18:50 +0700

    update

--- lib/Page/BacaanSholat.dart
+++ lib/Page/BacaanSholat.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
 
-class bacaansholat extends StatefulWidget{
-  bacaansholat ({super.key});
+class bacaansholat extends StatelessWidget{
+  const bacaansholat ({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(hexColor("#334E85")),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  onTap: (){
+                    Navigator.pop(context);
+                  },
+                  child: Icon(Icons.arrow_back_ios_new_outlined,
+                    size: 25,color:Colors.white ,),
+                ),
+              ],
+            ),
+            SizedBox(
+              child: Image.asset("lib/Image/bacaansholat.png"),
+            )
+          ],
+        ),
+      ),
+    );
   }
+}
+int hexColor(String color) {
+  String newColor = '0xff' + color;
+  newColor = newColor.replaceAll('#', '');
+  int finalColor = int.parse(newColor);
+  return finalColor;
 }
